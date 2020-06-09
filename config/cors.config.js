f68d1159fd9f099bbdf5986571f731f322091319
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-06-09 12:33:12 +0200

    cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-	origin: process.env.REACT_APP_API_URL,
+	origin: process.env.REACT_APP_API_URL || "http://localhost:3000",
 	allowedHeaders: ["Content-Type", "Origin"],
 	credentials: true,
 });
